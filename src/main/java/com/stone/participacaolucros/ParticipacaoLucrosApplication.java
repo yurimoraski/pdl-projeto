a80cff0e22594caa35ed41e030e65ebb3dc70f6c
Yuri Batista <yuri.moraski@gmail.com>
2021-03-08 19:38:18 -0300

    inicializa projeto

---
+++ src/main/java/com/stone/participacaolucros/ParticipacaoLucrosApplication.java
@@ -0,0 +1,13 @@
+package com.stone.participacaolucros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticipacaoLucrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticipacaoLucrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stone/participacaolucros/ParticipacaoLucrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.participacaolucros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParticipacaoLucrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
